7c87873c3caf81df6471953b3cc48ffc3e21fc61
TheShahnawaaz <107462651+TheShahnawaaz@users.noreply.github.com>
2023-08-05 18:11:18 +0530

    .

--- input.php
+++ input.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 
 $name = $_REQUEST['name'];
 $number = $_REQUEST['Number'];
@@ -15,6 +15,32 @@ else
     mail("kraza9798@gmail.com", "Data", $name $number $email $City $Add , "From: $name <$email>");
     echo "<script type='text/javascript'>alert('Your Message Sent Successfully!!');
     window.history.log(-1);
-    </script>"; 
+    </script>";
+}
+?> -->
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['Name'];
+    $number = $_POST['Number'];
+    $email = $_POST['Email'];
+    $category = $_POST['Category'];
+    $city = $_POST['City'];
+    $zipPostalCode = $_POST['ZipPostalCode'];
+    $message = $_POST['Message'];
+
+    if (empty($name) || empty($number) || empty($email) || empty($category) || empty($city) || empty($zipPostalCode) || empty($message)) {
+        echo "Please Fill All The Fields!!";
+    } else {
+        $to = "your_email@example.com"; // Replace with your email address
+        $subject = "Form Submission";
+        $body = "Name: $name\nNumber: $number\nEmail: $email\nCategory: $category\nCity: $city\nZip/Postal Code: $zipPostalCode\nMessage: $message";
+
+        if (mail($to, $subject, $body, "From: $name <$email>")) {
+            echo "<script type='text/javascript'>alert('Your Message Sent Successfully!!'); window.history.back();</script>";
+        } else {
+            echo "Sorry, there was an error sending your message.";
+        }
+    }
 }
 ?>
